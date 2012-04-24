fc09b24ce61f05e1dfc665dad0549eaa4c114590
sujal shah <codesujal@gmail.com>
2012-04-24 00:33:52 -0400

    forgot to tweak for heroku PORT variable

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
