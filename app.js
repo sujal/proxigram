6a3f3d403f6077e625343854c0ba6ae2f7d049a4
sujal shah <codesujal@gmail.com>
2012-04-24 09:29:04 -0400

    framework for remaining bits

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , wizard = require('./wizard');
 var lessMiddleware = require('less-middleware');
 
 var app = module.exports = express.createServer();
@@ -20,24 +21,51 @@ app.configure(function(){
       }));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
+  app.use(express.cookieParser());
   app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
 });
 
 app.configure('development', function(){
+  // app.use(express.errorHandler());
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+// 
+// app.configure('production', function(){
+//   app.use(express.errorHandler());
+// });
+
+function NotFound(msg) {
+	this.name = 'NotFound';
+	Error.call(this, msg);
+	Error.captureStackTrace(this, arguments.callee);
+}
+NotFound.prototype.__proto__ = Error.prototype;
+
 
 // Routes
 
 app.get('/', routes.index);
 app.get('/about', routes.about);
+app.get('/step1', wizard.step1);
+app.get('/step2', wizard.step2);
+
+app.get('*', function(req, res) {
+	throw new NotFound('Page not found.');
+});
 
 var port = process.env.PORT || 3000
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+
+// Custom 404 stuff
+app.error(function(err, req, res, next){
+    if (err instanceof NotFound) {
+        res.render('404.jade', { title: "That link was broken!" });
+    } else {
+        res.render('error.jade', { title: "Whoa! Error!", error: err });
+    }
+});
+
---
+++ wizard/index.js
@@ -0,0 +1,9 @@
+
+
+exports.step1 = function(req, res){
+  res.render('step1', { title: 'Step 1: Sign in to Instagram' })
+};
+
+exports.step2 = function(req, res){
+  res.render('step2', { title: 'Step 2: Grab your JS and Go!' })
+};
