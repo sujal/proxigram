e3707c825b1b264d46e95fe407b5ac0037d35034
sujal shah <codesujal@gmail.com>
2012-04-26 14:47:15 -0400

    simple site updates

--- app/controllers/misc-controller.js
+++ app/controllers/misc-controller.js
@@ -6,11 +6,14 @@
 module.exports = function (app) {
   
   app.get('/', function(req, res){
-    res.render('misc/index', { title: 'A simple way to get your Instagram photos' })
+    res.render('misc/index', { title: 'A simple way to get your Instagram photos' });
   });
   
   app.get('/about', function(req, res){
-    res.render("misc/about", { title: 'About Proxigram' })
+    res.render("misc/about", { title: 'About Proxigram' });
   });
   
+  app.get('/faq', function(req, res){
+    res.render("misc/faq", { title: "FAQ" });
+  });
 }
