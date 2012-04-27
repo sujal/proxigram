770d4fe3b48f8188c9725efed2dd75e756b89c1d
sujal shah <codesujal@gmail.com>
2012-04-27 12:04:03 -0400

    make dashboard the primary page for logged in users
    also, move halflings to cloudfront

--- app/controllers/misc-controller.js
+++ app/controllers/misc-controller.js
@@ -6,7 +6,11 @@
 module.exports = function (app) {
   
   app.get('/', function(req, res){
-    res.render('misc/index', { title: 'A simple way to get your Instagram photos' });
+    if (req.isAuthenticated()) {
+      res.redirect('/code');
+    } else {
+      res.render('misc/index', { title: 'A simple way to get your Instagram photos' });
+    }
   });
   
   app.get('/about', function(req, res){
