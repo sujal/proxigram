05d91bd01c8ba6431c58c03276a9530ed0bb834e
sujal shah <codesujal@gmail.com>
2012-05-01 13:04:12 -0400

    buttons & route prep

--- app/controllers/users-controller.js
+++ app/controllers/users-controller.js
@@ -3,12 +3,31 @@ var moment = require('moment');
 
 module.exports = function(app) {
   
+ app.get('/code', ensureAuthenticated, function(req, res) {
+    res.render('users/code', { title: 'Your Dashboard' });
+  });
+  
+  app.get('/users/refresh', ensureAuthenticated, function(req, res) {
+    ImageList.instagramPhotosForUser(req.user, {force_refresh: true}, function(err, imageList){
+      if (err) {
+        req.flash("error", "There was an error refreshing the feed.");
+      } else {
+        req.flash("info", "Successfully refreshed feed. New timestamp is: " + moment(imageList.updated_at).format('MM/DD/YYYY h:mm:ss a'));        
+      }
+      res.redirect('/code');      
+    });
+  });
+  
+  
+  // AUTHENTICATION ROUTES
+
   // Conventions are as follows:
   // /auth/* routes are used for *creating* accounts and *login*
   // /connect/* routes are used for *adding* connections to existing accounts.
   //            This call requires login to make the connection.
   
   /* INSTAGRAM calls */
+  
   app.get('/auth/instagram',
     passport.authenticate('instagram', {successFlash: true, failureFlash: true}),
     function(req, res){
@@ -29,25 +48,4 @@ module.exports = function(app) {
   });
   
   
-  
-  
-  
-  
-  
-  
-  
-  app.get('/code', ensureAuthenticated, function(req, res) {
-    res.render('users/code', { title: 'Your Dashboard' })
-  });
-  
-  app.get('/users/refresh', ensureAuthenticated, function(req, res) {
-    ImageList.instagramPhotosForUser(req.user, {force_refresh: true}, function(err, imageList){
-      if (err) {
-        req.flash("error", "There was an error refreshing the feed.");
-      } else {
-        req.flash("info", "Successfully refreshed feed. New timestamp is: " + moment(imageList.updated_at).format('MM/DD/YYYY h:mm:ss a'));        
-      }
-      res.redirect('/code');      
-    });
-  });
-}
+};
