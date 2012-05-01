256bc350d4df3f429819f2606204adcb0c027f2b
sujal shah <codesujal@gmail.com>
2012-05-01 01:34:08 -0400

    boom

--- app/controllers/admin-controller.js
+++ app/controllers/admin-controller.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var ImageList = mongoose.model("ImageList");
+var User = mongoose.model("User");
 
 module.exports = function(app) {
   
@@ -16,6 +17,38 @@ module.exports = function(app) {
   
   app.get('/admin(/index)?', ensureAuthenticated, ensureAdmin, function(req, res){
     res.render('admin/index', { title: "Admin Section" });    
+  });
+  
+  app.get('/admin/migrations/schema_change', ensureAuthenticated, ensureAdmin, function(req, res){
+    
+    var names = [];
+    
+    User.find({},function(err, users) {
+      var total = users.length;
+      for (var i = total - 1; i >= 0; i--){
+        var user = users[i];
+        if (user.tokens.instagram.account_id == null) {
+          user.tokens.instagram.account_id = user.instagram_id;
+          user.tokens.instagram.display_name = user.displayName;
+          user.tokens.instagram.token = user.oauth_token;
+          user.save(function(err, user){
+            if (err) {
+              throw err;
+            }
+            names.push(user.displayName);
+            total--;
+            if (total <= 0)
+            {
+              // means that everything has returned
+              console.log("saved "+user.tokens.instagram.display_name);
+              res.send({meta: 200, data: names}, 200);
+            }
+          });
+        } else {
+          console.log("account already migrated (or at least had something in the tokens collection)");
+        }
+      };
+    });
   });
   
 }
