54226df9a9b79283224ccfbcbf9ebb74caa3d158
sujal shah <codesujal@gmail.com>
2012-05-04 09:02:58 -0400

    getting ready to test the PuSH helper

---
+++ app/controllers/facebook-realtime-controller.js
@@ -0,0 +1,16 @@
+var User = mongoose.model("User")
+  , ImageList = mongoose.model("ImageList")
+  , PuSHHelper = require('node-push-helper').PuSHHelper;
+
+module.exports = function (app) {
+
+  // a GET request will be a challenge query
+  app.get('/facebook/realtime', function(req, res){
+    PuSHHelper.handshake(req, res);
+  });
+  
+  // this is where Instagram will send updates (using POST)
+  app.post('/facebook/realtime', PuSHHelper.verifier(config.facebook.app_secret), function(req, res){
+    
+  });
+}
--- app/controllers/instagram-realtime-controller.js
+++ app/controllers/instagram-realtime-controller.js
@@ -1,52 +1,47 @@
 var User = mongoose.model("User")
-  , ImageList = mongoose.model("ImageList");
+  , ImageList = mongoose.model("ImageList")
+  , PuSHHelper = require('node-push-helper').PuSHHelper;
 
 module.exports = function (app) {
   
   // a GET request will be a challenge query
   app.get('/instagram/realtime', function(req, res){
-    Instagram.subscriptions.handshake(req, res);
+    PuSHHelper.handshake(req, res);
   });
   
   // this is where Instagram will send updates (using POST)
-  app.post('/instagram/realtime', function(req, res){
-    console.log("Received a notification");
-    if (Instagram.subscriptions.verified(req)) {
+  app.post('/instagram/realtime', PuSHHelper.verifier(config.instagram.client_secret), function(req, res){
+    console.log("Received & verified a notification");
+    
+    // loop through incoming updates and ping the API to update
+    // the cached records
+    var notifications = req.body;
+    
+    for (var i=0; i < notifications.length; i++) {
+      var update = notifications[i];
+      if (update["object"] == "user" && update["changed_aspect"] == "media") {
+        User.findOne({"tokens.instagram.account_id": update["object_id"]}, function(err, user){
+          if (err == null) {
+            ImageList.instagramPhotosForUser(user, {force_refresh: true}, function(err, imageList){
+              if (err == null) {
+                // do nothing, I think?
+                console.log("SUCCESS refresh of feed for " + user.instagram_id + "/" + user.instagram_name);
+              } else {
+                // fail
+                console.log("ERROR refreshing feed on notification: " + err);
+              }
+            })
+          } else {
+            // failed
+            console.log("ERROR handling notification: " + err);
+          }
+        });
+      }
+    }; // end for loop
+    
+    // we can tell Instagram we're good to go because updates will happen async
+    res.send({meta: 200, message: "Received and understood."}, 200);
       
-      console.log("verified notification");
-      
-      // loop through incoming updates and ping the API to update
-      // the cached records
-      var notifications = req.body;
-      
-      for (var i=0; i < notifications.length; i++) {
-        var update = notifications[i];
-        if (update["object"] == "user" && update["changed_aspect"] == "media") {
-          User.findOne({"tokens.instagram.account_id": update["object_id"]}, function(err, user){
-            if (err == null) {
-              ImageList.instagramPhotosForUser(user, {force_refresh: true}, function(err, imageList){
-                if (err == null) {
-                  // do nothing, I think?
-                  console.log("SUCCESS refresh of feed for " + user.instagram_id + "/" + user.instagram_name);
-                } else {
-                  // fail
-                  console.log("ERROR refreshing feed on notification: " + err);
-                }
-              })
-            } else {
-              // failed
-              console.log("ERROR handling notification: " + err);
-            }
-          });
-        }
-      }; // end for loop
-      
-      // we can tell Instagram we're good to go because updates will happen async
-      res.send({meta: 200, message: "Received and understood."}, 200);
-      
-    } else {
-      res.send({meta: 403, message: "Verification failed"}, 403);
-    }
   });
   
 }
