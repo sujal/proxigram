91b7ded519163f474ee76c623cfad6a94c28c3dc
sujal shah <codesujal@gmail.com>
2012-05-08 09:20:06 -0400

    null check

--- app/controllers/api-controller.js
+++ app/controllers/api-controller.js
@@ -12,17 +12,20 @@ module.exports = function(app) {
       var user = req.user
       var force_refresh = req.param("force_refresh", "false") == "true";
       ImageList.instagramPhotosForUser(user, {force_refresh: force_refresh}, function(err, imageList){
-        var limit = Number(req.param("limit", "30"));
-        var include_raw = req.param("include_raw", "no");
+        var outputObj = null;
+        if (imageList != null) {
+          var limit = Number(req.param("limit", "30"));
+          var include_raw = req.param("include_raw", "no");
 
-        var outputObj = imageList.toObject();
-        
-        if (limit < outputObj.images.length) {
-          outputObj.images.splice(limit,outputObj.images.length-limit);
-        }
-        if (include_raw == "no" || include_raw == "false" || include_raw === false) {
-          for (var i = outputObj.images.length - 1; i >= 0; i--){
-            outputObj.images[i].raw_json = undefined;
+          outputObj = imageList.toObject();
+
+          if (limit < outputObj.images.length) {
+            outputObj.images.splice(limit,outputObj.images.length-limit);
+          }
+          if (include_raw == "no" || include_raw == "false" || include_raw === false) {
+            for (var i = outputObj.images.length - 1; i >= 0; i--){
+              outputObj.images[i].raw_json = undefined;
+            }          
           }          
         }
         res.send({meta: 200, data: outputObj}, 200);
