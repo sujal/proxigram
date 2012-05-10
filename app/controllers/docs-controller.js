101122d62fb42d7258d89d4f41d5e1a5fa79fc3e
sujal shah <codesujal@gmail.com>
2012-05-09 23:36:02 -0400

    add jquery documentation page

--- app/controllers/docs-controller.js
+++ app/controllers/docs-controller.js
@@ -8,4 +8,8 @@ module.exports = function(app) {
     res.render('docs/photos', { title: "Documentation for /api/photos" });
   });
   
+  app.get('/docs/jquery-plugin', function(req, res){
+    res.render('docs/jquery-plugin', { title: "Documentation for the jQuery plugin" });
+  });
+  
 }
