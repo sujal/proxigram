535faa5ff325219cf90c8967dd4587cb3cd53b9e
sujal shah <codesujal@gmail.com>
2012-05-10 10:00:43 -0400

    actually use the flickr notifications now

--- app/controllers/flickr-realtime-contoller.js
+++ app/controllers/flickr-realtime-contoller.js
@@ -16,7 +16,30 @@ module.exports = function (app) {
     var notifications = req.body;
     console.log("FLICKR NOTIFICATION: " + util.inspect(notifications));
     
+    // fire this, but don't hold up the reply to flickr
+    ImageList.refreshFeedForUserProvider(req.user, "flickr", function(err, imageList) {
+      if (err) {
+        console.log("ERROR: Flickr notifiication had an error refreshing the feed: " + err);
+      } else {
+        console.log("SUCCESS: Flickr notification successfully refreshed feed. New timestamp is: " + moment(imageList.updated_at).format('MM/DD/YYYY h:mm:ss a'));
+      }
+    });
+    
+    
     // we can tell Flickr we're good to go because updates will happen async
     res.send({meta: 200, message: "Received and understood."}, 200);
   });
+  
+  app.get('/flickr/subscriptions', ensureAuthenticated, function(req, res){
+    var client = req.user.flickrClient();
+    if (client) {
+      client.executeAPIRequest("flickr.push.getSubscriptions", {
+                                }, true, function(err, response){
+                                  if (err) { throw err; }
+                                  console.log("result is " + response);
+                                  res.send( response, 200);
+                                });
+    }
+    
+  })
 }
