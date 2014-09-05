b73ef1281820752c724675fb6506d1d82709ac68
sujal shah <codesujal@gmail.com>
2014-09-04 23:55:47 -0400

    update for SSL

--- config/index.js
+++ config/index.js
@@ -23,11 +23,11 @@ config = {
 if (process.env.NODE_ENV == undefined || process.env.NODE_ENV == "development") {
   config.instagram.oauth_callback_url = "http://proxigram.dev/auth/instagram/callback";
   config.instagram.connect_oauth_callback_url = "http://proxigram.dev/connect/instagram/callback";
-  // this needs to be an env var b/c depends on how you can route traffic to your 
-  // dev box from instagram's servers. For example, i use DynDNS and route specific 
+  // this needs to be an env var b/c depends on how you can route traffic to your
+  // dev box from instagram's servers. For example, i use DynDNS and route specific
   // ports to my dev box.
   config.instagram.realtime_callback_url = process.env.REALTIME_CALLBACK_URL;
-  
+
   config.flickr.oauth_callback_url = "http://proxigram.dev/auth/flickr/callback";
   config.flickr.connect_oauth_callback_url = "http://proxigram.dev/connect/flickr/callback";
   config.flickr.push_callback_url = process.env.FLICKR_PUSH_CALLBACK_URL;
@@ -35,19 +35,19 @@ if (process.env.NODE_ENV == undefined || process.env.NODE_ENV == "development") 
   config.facebook.oauth_callback_url = "http://proxigram.dev/auth/facebook/callback";
   config.facebook.connect_oauth_callback_url = "http://proxigram.dev/connect/facebook/callback";
   config.facebook.push_callback_url = process.env.FACEBOOK_PUSH_CALLBACK_URL;
-   
+
 } else {
   // some other environment
-  config.instagram.oauth_callback_url = "http://proxigram.com/auth/instagram/callback"; 
-  config.instagram.connect_oauth_callback_url = "http://proxigram.com/connect/instagram/callback"; 
+  config.instagram.oauth_callback_url = "https://www.proxigram.com/auth/instagram/callback";
+  config.instagram.connect_oauth_callback_url = "https://www.proxigram.com/connect/instagram/callback";
   config.instagram.realtime_callback_url = process.env.REALTIME_CALLBACK_URL;
 
-  config.flickr.oauth_callback_url = "http://proxigram.com/auth/flickr/callback";
-  config.flickr.connect_oauth_callback_url = "http://proxigram.com/connect/flickr/callback";
+  config.flickr.oauth_callback_url = "https://www.proxigram.com/auth/flickr/callback";
+  config.flickr.connect_oauth_callback_url = "https://www.proxigram.com/connect/flickr/callback";
   config.flickr.push_callback_url = process.env.FLICKR_PUSH_CALLBACK_URL;
 
-  config.facebook.oauth_callback_url = "http://proxigram.com/auth/facebook/callback";
-  config.facebook.connect_oauth_callback_url = "http://proxigram.com/connect/facebook/callback";
+  config.facebook.oauth_callback_url = "https://www.proxigram.com/auth/facebook/callback";
+  config.facebook.connect_oauth_callback_url = "https://www.proxigram.com/connect/facebook/callback";
   config.facebook.push_callback_url = process.env.FACEBOOK_PUSH_CALLBACK_URL;
 
 }
