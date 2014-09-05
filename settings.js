765dd588bb53c9425e1a96ebc7ad557d1f4c7be3
sujal shah <codesujal@gmail.com>
2014-09-05 00:30:29 -0400

    iupdate for 2.x

--- settings.js
+++ settings.js
@@ -31,8 +31,9 @@ Instagram.set('callback_url', config.instagram.realtime_callback_url);
 Instagram.set('maxSockets', 10);
 
 forceSSL = function(req, res, next){
-  if(!req.secure){
-    var httpsPort = req.app.get('httpsPort') || 443;
+  if (!(req.connection.server instanceof tls.Server  // for Express 2.x
+      || req.socket.socket.server instanceof tls.Server  // for Express 3.x
+      || 'https' === req.headers['x-forwarded-proto'])) {
     var fullUrl = parseUrl('http://' + req.header('Host') + req.url);
     res.redirect('https://' + fullUrl.hostname + req.url, 301);
   } else {
