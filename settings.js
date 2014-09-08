29c345cd2c38b6120c1b8af0344bdf4a26c84459
sujal shah <codesujal@gmail.com>
2014-09-08 00:05:58 -0400

    disable SSL req in dev

--- settings.js
+++ settings.js
@@ -84,7 +84,9 @@ function bootApplication(app) {
                               secret: (process.env.SESSION_SECRET || "sssh sssh ssshhhhhhh") }));
     app.use(passport.initialize());
     app.use(passport.session());
-    app.use(forceSSL);
+    if (!(process.env.NODE_ENV == undefined || process.env.NODE_ENV == "development")) {
+      app.use(forceSSL);
+    }
     app.use(express.static(__dirname + '/public', {maxAge: 86400000}));
     app.use(express.logger(':method :url :status'));
     app.use(app.router);
